248a8a8fd832dd9a82dcd74b430ed7506d13d8a1
Elenflora <elenfloraby@gmail.com>
2025-05-26 18:04:31 +0530

    Add files via upload

---
+++ Homepage1.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web.UI;
+
+namespace Homepage1
+{
+    public partial class Homepage1 : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                // Code to execute on first page load (if needed)
+            }
+        }
+
+        // Example method: Redirect to a page when a button is clicked (if applicable)
+        protected void RedirectToPage(object sender, EventArgs e)
+        {
+            Response.Redirect("TargetPage.aspx"); // Change to the appropriate target page
+        }
+    }
+}
